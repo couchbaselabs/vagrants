6a25f6b384188284e6241ba7b2a05363fa37a0cc
Dave Rigby <daver@couchbase.com>
2014-06-03 12:25:35 +0100

    Expand Ubuntu 12.04 build config to for 2.5.1
    2.5.1 (and older) use autotools to build; along with a couple of other
    things. Add them.

--- cbdev_ubuntu_1204/manifests/default.pp
+++ cbdev_ubuntu_1204/manifests/default.pp
@@ -16,7 +16,24 @@ package { $build_deps:
         require => Exec["ppa_cmake"]
 }
 
+# Needed for pre-cmake (<3.0.0) builds:
+$old_build_deps = [ "automake", "libtool", "libunwind7-dev", "cloog-ppl" ]
+package { $old_build_deps:
+        ensure => "installed"
+}
 exec {"/usr/bin/curl https://storage.googleapis.com/git-repo-downloads/repo > /usr/local/bin/repo && chmod a+x /usr/local/bin/repo":
      alias => "install_repo",
      creates => "/usr/local/bin/repo"
+}
+
+# Google perftools (for tcmalloc). Not sure if this is needed for 3.0.0+
+exec {"/usr/bin/curl -O https://gperftools.googlecode.com/files/gperftools-2.1.tar.gz":
+     alias   => "download_gperftools",
+     cwd     => "/tmp",
+     creates => "/tmp/gperftools-2.1.tar.gz"
+}
+exec {"/bin/tar xfz gperftools-2.1.tar.gz && cd gperftools-2.1 && ./configure && make -j4 && make install":
+     alias => "install_gperftools",
+     cwd =>  "/tmp",
+     require => Exec["download_gperftools"]
 }
