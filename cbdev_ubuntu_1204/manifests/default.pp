0d44d9e027f5cd0025af2bbc94a96195b953d318
Dave Rigby <daver@couchbase.com>
2014-06-03 16:59:56 +0100

    Update Ubuntu 12.04 build config dependancies
    Previously provisioning may fail due to not listing dependancies of gperftools explicitly.

--- cbdev_ubuntu_1204/manifests/default.pp
+++ cbdev_ubuntu_1204/manifests/default.pp
@@ -1,6 +1,19 @@
-package { 'python-software-properties':
-        ensure  =>  latest,
+exec { "/usr/bin/apt-get update":
+    alias   => "apt-get-update",
 }
+
+# Needed for tcmalloc & Couchbase
+package { ['build-essential', 'libunwind7-dev']:
+        require => Exec["apt-get-update"],
+        ensure  =>  latest
+}
+
+# Needed for apt-add-repository
+package { ['python-software-properties']:
+        require => Exec["apt-get-update"],
+        ensure  =>  latest
+}
+
 exec { "/usr/bin/apt-add-repository ppa:yjwong/cmake && /usr/bin/apt-get update":
     alias   => "ppa_cmake",
     require => Package["python-software-properties"],
@@ -8,32 +21,34 @@ exec { "/usr/bin/apt-add-repository ppa:yjwong/cmake && /usr/bin/apt-get update"
 }
 
 # Needed for building couchbase
-$build_deps = [ "build-essential", "git", "cmake", "libevent-dev",
-                "libcurl4-openssl-dev", "libicu-dev", "libsnappy-dev",
-                "libv8-dev", "erlang", "erlang-src" ]
-package { $build_deps:
+$couchbase_deps = [ "git", "cmake", "libevent-dev", "libcurl4-openssl-dev",
+                "libicu-dev", "libsnappy-dev", "libv8-dev", "erlang",
+                "erlang-src" ]
+package { $couchbase_deps:
         ensure => "installed",
         require => Exec["ppa_cmake"]
 }
 
 # Needed for pre-cmake (<3.0.0) builds:
-$old_build_deps = [ "automake", "libtool", "libunwind7-dev", "cloog-ppl" ]
+$old_build_deps = [ "automake", "libtool", "cloog-ppl" ]
 package { $old_build_deps:
         ensure => "installed"
 }
-exec {"/usr/bin/curl https://storage.googleapis.com/git-repo-downloads/repo > /usr/local/bin/repo && chmod a+x /usr/local/bin/repo":
+exec {"/usr/bin/wget -O- https://storage.googleapis.com/git-repo-downloads/repo > /usr/local/bin/repo && chmod a+x /usr/local/bin/repo":
      alias => "install_repo",
      creates => "/usr/local/bin/repo"
 }
 
 # Google perftools (for tcmalloc). Not sure if this is needed for 3.0.0+
-exec {"/usr/bin/curl -O https://gperftools.googlecode.com/files/gperftools-2.1.tar.gz":
+exec {"/usr/bin/wget https://gperftools.googlecode.com/files/gperftools-2.1.tar.gz":
      alias   => "download_gperftools",
      cwd     => "/tmp",
      creates => "/tmp/gperftools-2.1.tar.gz"
 }
-exec {"/bin/tar xfz gperftools-2.1.tar.gz && cd gperftools-2.1 && ./configure && make -j4 && make install":
+exec {"/bin/tar --overwrite -x -z -f gperftools-2.1.tar.gz && cd gperftools-2.1 && ./configure && make -j4 && make install":
      alias => "install_gperftools",
      cwd =>  "/tmp",
-     require => Exec["download_gperftools"]
+     require => [ Exec["download_gperftools"],
+                  Package["build-essential"],
+                  Package["libunwind7-dev"] ]
 }
