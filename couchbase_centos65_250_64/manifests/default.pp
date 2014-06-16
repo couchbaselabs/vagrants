8acf81d8a7f94f7392a0e6382189c504b1e57c74
Ian McCloy <ian.mccloy@couchbase.com>
2014-06-16 15:36:36 +0100

    Added centos 6.5 cb 2.5.0 cluster

---
+++ couchbase_centos65_250_64/manifests/default.pp
@@ -0,0 +1,41 @@
+# ===
+# Install and Run Couchbase Server
+# ===
+
+$version = "2.5.0"
+$stem = "couchbase-server-enterprise_${version}_x86_64"
+$suffix = $operatingsystem ? {
+    Ubuntu => ".deb",
+    CentOS => ".rpm",
+}
+$filename = "$stem$suffix"
+
+# Download the Sources
+exec { "couchbase-server-source":
+    command => "/usr/bin/wget http://packages.couchbase.com/releases/$version/$filename",
+    cwd => "/vagrant/",
+    creates => "/vagrant/$filename",
+    before => Package['couchbase-server']
+}
+
+# Install Couchbase Server
+package { "couchbase-server":
+    provider => $operatingsystem ? {
+        Ubuntu => dkpg,
+        CentOS => rpm,
+    },
+    ensure => installed,
+    source => "/vagrant/$filename",
+}
+
+# Ensure firewall is off (some CentOS images have firewall on by default).
+service { "iptables":
+    ensure => "stopped",
+    enable => false
+}
+
+# Ensure the service is running
+service { "couchbase-server":
+	ensure => "running",
+	require => Package["couchbase-server"]
+}
