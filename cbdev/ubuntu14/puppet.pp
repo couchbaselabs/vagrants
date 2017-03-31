6603e5f085ab94589979d1c4c913cc6650efc647
David Haikney <david.haikney@couchbase.com>
2017-03-31 14:37:19 +0100

    beefier build VM and enhancements to build release packages (#51)

--- cbdev/ubuntu14/puppet.pp
+++ cbdev/ubuntu14/puppet.pp
@@ -5,7 +5,8 @@ exec { "/usr/bin/apt-get update":
 # Needed for building couchbase
 $couchbase_deps = [ "git", "cmake", "libevent-dev", "libcurl4-openssl-dev", "gccgo-go",
                 "libicu-dev", "libsnappy-dev", "libv8-dev", "erlang",
-                "erlang-src", "build-essential", "libgoogle-perftools-dev" ]
+                "erlang-src", "build-essential", "libgoogle-perftools-dev",
+	 	"devscripts", "debhelper", "dh-virtualenv" ]
 package { $couchbase_deps:
         ensure => "installed"
 }
@@ -14,6 +15,18 @@ package { $couchbase_deps:
 $old_build_deps = [ "automake", "libtool", "cloog-ppl" ]
 package { $old_build_deps:
         ensure => "installed"
+}
+
+# Some of the build tools expect a writeable /opt/couchbase directory
+file { '/opt/couchbase':
+     ensure => 'directory',
+     owner  => 'vagrant',
+     group  => 'vagrant',
+     mode   => '0750',
+}
+ file { '/home/vagrant/.gitconfig':
+	ensure => 'present',
+	source => 'file:///vmhost_home/.gitconfig'
 }
 
 exec {"/usr/bin/wget -O- https://storage.googleapis.com/git-repo-downloads/repo > /usr/local/bin/repo && chmod a+x /usr/local/bin/repo":
