2add7c2b1fc3ee7ec3258ae84d9ab8b404820b81
Chris Malarky <chris@malarky.org>
2021-07-21 23:48:31 +0100

    CBSP-3474: Add changes missing in previous commits (#165)

--- puppet.pp
+++ puppet.pp
@@ -100,7 +100,8 @@ package { "libssl":
         'Debian' => $::operatingsystemmajrelease ? {
 	    '7' => "libssl1.0.0",
 	    '8' => "libssl1.0.0",
-	    '9' => "libssl1.1"},
+	    '9' => "libssl1.1",
+	    '10' => "libssl1.1"},
         'OpenSuSE' => "openssl",
     },
     ensure => present,
